8ebfca2909eb4005e862ed1450ab41d04e6deed2
Marcos Vinícius Silva <m_vinicius94@outlook.com>
2023-01-17 07:21:35 -0300

    criando autonomia para definir o maior e menor numero

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,7 +1,14 @@
+const menorNumero = 1;
+const maiorNumero = 1000;
 const numeroSecreto = numeroAleatorio()
 
 function numeroAleatorio() {
-    return parseInt(Math.random() * 100)
+    return parseInt(Math.random() * maiorNumero + 1)
 }
 
-console.log('Numero Secreto: ', numeroSecreto)
+console.log('Numero Secreto: ', numeroSecreto);
+
+const elementoMenorNumero = document.querySelector("#numero-menor");
+const elementoMaiorNumero = document.querySelector("#numero-maior");
+elementoMaiorNumero.innerHTML = maiorNumero;
+elementoMenorNumero.innerHTML = menorNumero;
